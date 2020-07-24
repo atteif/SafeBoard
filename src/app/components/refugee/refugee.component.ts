0a03e0143c1e4c84c4cbd45082a2f6bfe27bf82b
oferjani <oumayma.ferjani@esprit.tn>
2020-07-24 19:49:11 +0100

    refugee changes

---
+++ src/app/components/refugee/refugee.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { RefugeeService } from './services/refugee.service';
+import { Refugee } from './models/Refugee';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-refugee',
+  templateUrl: './refugee.component.html',
+  styleUrls: ['./refugee.component.css']
+})
+export class RefugeeComponent implements OnInit {
+  headers;
+  refugees: Refugee[] = [];
+  spresp: any;
+  ref : Refugee;
+  sum : any[] = [[]];
+
+  constructor(private service: RefugeeService , private routes: Router) { }
+
+  ngOnInit(): void {
+    this.getAllRefugees();
+    this.getSum();
+    console.log(this.sum);
+  }
+
+  getAllRefugees() {
+    this.service.getRef().subscribe(resp =>  {
+      const keys = resp.headers.keys();
+      this.headers = keys.map(key =>
+        `${key}: ${resp.headers.get(key)}`);
+        for (const data of resp.body) {
+        this.refugees.push(data);        }
+        });
+    console.log(this.refugees);
+  }
+  addRefugee() {
+    this.routes.navigateByUrl('/newRefugee');
+  }
+
+    isComplete(ref) {
+    console.log(ref);
+    console.log(ref.is_complete);
+    ref.is_complete = true;
+      this.service
+          .updateRef(ref)
+          .subscribe(resp => {
+            return this.spresp.push(resp);
+          });
+      console.log(ref.is_complete);
+      this.routes.navigateByUrl('/refugee')
+    }
+
+  getSum() {
+    this.service.getSum().subscribe(resp =>  {
+      const keys = resp.headers.keys();
+      console.log(keys);
+      this.headers = keys.map(key =>
+          `${key}: ${resp.headers.get(key)}`);
+      for (const data of resp.body) {
+        this.sum.push(data);        }
+    });
+  }
+
+}
+
