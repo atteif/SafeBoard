64244b7acf5a6a91a05be8794ed8e8ec175b81ce
MariemZ18 <mariam.zoghlami.103@gmail.com>
2020-07-24 02:33:05 +0200

    Added event and users management

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,16 +10,23 @@ import { ProfileComponent } from './examples/profile/profile.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 import { LostandfoundComponent } from './components/lostandfound/lostandfound.component';
 import { NewlostandfoundComponent } from './components/lostandfound/newlostandfound/newlostandfound.component';
+import {EventComponent} from './components/event/event.component';
+import {AddEventComponent} from './components/add-event/add-event.component';
+import {UsersComponent} from './components/users/users.component';
+import {AddUserComponent} from './components/add-user/add-user.component';
 
 const routes: Routes =[
     { path: '', redirectTo: 'refugees', pathMatch: 'full' },
     { path: 'refugees',                component: ComponentsComponent },
-    { path: 'event',          component: NucleoiconsComponent },
+    { path: 'event',          component: EventComponent },
     { path: 'donation',     component: LandingComponent },
     { path: 'lostfound',       component: LostandfoundComponent },
     { path: 'newlostfound',       component: NewlostandfoundComponent },
     { path: 'association',     component: ProfileComponent },
-    { path: 'aaaa',       component: LostandfoundComponent }
+    { path: 'aaaa',       component: LostandfoundComponent },
+    { path: 'addEvent',       component: AddEventComponent },
+    { path: 'users',       component: UsersComponent },
+    { path: 'addUser',       component: AddUserComponent }
 ];
 
 @NgModule({
